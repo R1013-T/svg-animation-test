a16744e1e2942f292174c27fc72a2dec4a3f850c
R1013-T <nosuke1013@icloud.com>
2024-04-07 21:09:30 +0900

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,136 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="h-dvh overflow-y-scroll">
+      <div className="h-dvh w-dvw overflow-hidden relative mb-10">
+        <svg
+          viewBox="0 0 200 200"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute h-dvh w-dvw"
+        >
+          <filter id="displacementFilter">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="1"
+              numOctaves="300"
+              result="fractalNoise"
+            >
+              <animate
+                attributeName="baseFrequency"
+                from="3"
+                to="2"
+                dur="20s"
+                repeatCount="indefinite"
+              />
+            </feTurbulence>
+            <feDisplacementMap
+              in2="fractalNoise"
+              in="SourceGraphic"
+              scale="150"
+              xChannelSelector="R"
+              yChannelSelector="G"
             />
-          </a>
-        </div>
+          </filter>
+          <mask id="mask">
+            <circle cx="100" cy="100" r="100" style={{ fill: "#FFF" }} />
+          </mask>
+
+          <circle
+            cx="-50"
+            cy="240"
+            r="200"
+            style={{ fill: "#333333", filter: "url(#displacementFilter)" }}
+            mask="url(#mask)"
+          />
+        </svg>
+
+        <svg
+          viewBox="0 0 200 200"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute rotate-180 h-dvh w-dvw"
+        >
+          <filter id="displacementFilter">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="1"
+              numOctaves="300"
+              result="fractalNoise"
+            >
+              <animate
+                attributeName="baseFrequency"
+                from="2"
+                to="3"
+                dur="20s"
+                repeatCount="indefinite"
+              />
+            </feTurbulence>
+            <feDisplacementMap
+              in2="fractalNoise"
+              in="SourceGraphic"
+              scale="150"
+              xChannelSelector="R"
+              yChannelSelector="G"
+            />
+          </filter>
+          <mask id="mask">
+            <circle cx="100" cy="100" r="100" style={{ fill: "#FFF" }} />
+          </mask>
+
+          <circle
+            cx="-50"
+            cy="300"
+            r="200"
+            style={{ fill: "#333333", filter: "url(#displacementFilter)" }}
+            mask="url(#mask)"
+          />
+        </svg>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="border border-red-300 h-dvh w-dvw">
+        <svg
+          id="eALyGup1cVD1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 1280 720"
+          shapeRendering="geometricPrecision"
+          textRendering="geometricPrecision"
+          style={{ backgroundColor: "#000" }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <style>
+            {`#eALyGup1cVD2_to {animation: eALyGup1cVD2_to__to 8000ms linear infinite normal forwards}@keyframes eALyGup1cVD2_to__to { 0% {transform: translate(640px,360px)} 25% {transform: translate(370.961898px,360px)} 50% {transform: translate(640px,360px)} 75% {transform: translate(921.388761px,360px)} 100% {transform: translate(640px,360px)}}`}
+          </style>
+          <defs>
+            <linearGradient
+              id="eALyGup1cVD2-fill"
+              x1="0"
+              y1="0.5"
+              x2="1"
+              y2="0.5"
+              spreadMethod="pad"
+              gradientUnits="objectBoundingBox"
+              gradientTransform="translate(0 0)"
+            >
+              <stop id="eALyGup1cVD2-fill-0" offset="0%" stopColor="#FFFFFF" />
+              <stop
+                id="eALyGup1cVD2-fill-1"
+                offset="100%"
+                stopColor="#a0ff61"
+              />
+            </linearGradient>
+          </defs>
+          <g id="eALyGup1cVD2_to" transform="translate(640,360)">
+            <rect
+              width="2492.099859"
+              height="2223.719874"
+              rx="0"
+              ry="0"
+              transform="scale(0.820545,1) translate(-1246.049942,-1111.859937)"
+              fill="url(#eALyGup1cVD2-fill)"
+              strokeWidth="0"
+            />
+          </g>
+        </svg>
       </div>
     </main>
-  )
+  );
 }
